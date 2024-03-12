57accc271ea236765cb1b50f4054997bfd32de7b
MirlanFdeveloper <miki02101981@gmail.com>
2024-03-12 17:55:24 +0600

    added

---
+++ variables_sabak_dart_1/bin/variables_sabak_dart_1.dart
@@ -0,0 +1,34 @@
+void main() {
+
+String a = 'Мирлан';
+int b = 42;
+String f = 'жаштагы программист';
+int m = 100 ;
+String n = "дон ашык проект жасаган.";
+
+double c = 49;
+bool d = true;
+bool e = false;
+
+String name = "Miki\$";
+int age = 42;
+String info = " $name  $age";
+
+print('$a $b $f $m  $n');
+
+print(a);
+print(b);
+print(f);
+print('\$ $c');
+
+print(d);
+print(e);
+print(info);
+
+
+
+
+
+
+
+}
